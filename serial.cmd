5e6def1c0ea571887344dbbe614622fb486b2704
Peiyu Quan <41603034+PeiyuQuan@users.noreply.github.com>
2022-02-15 10:52:42 -0800

    Add files via upload

---
+++ serial.cmd
@@ -0,0 +1,22 @@
+#epicsEnvSet ("STREAM_PROTOCOL_PATH", "$(IP)/db")
+
+################################################################################
+# AJA power supply setup
+
+#drvAsynSerialPortConfigure("portName","ttyName",priority,noAutoConnect,noProcessEosIn)
+#asynSetOption("portName",addr,"Key","value")
+
+#drvAsynSerialPortConfigure("serial1", "192.168.0.10:4001<http://192.168.0.10:4001/> COM", 0, 0, 0)
+drvAsynSerialPortConfigure("serial1", "/dev/ttyUSB0", 0, 0, 0)
+asynSetOption("serial1",0,"baud","9600")
+asynSetOption("serial1",0,"bits","8")
+asynSetOption("serial1",0,"stop","1")
+asynSetOption("serial1",0,"parity","none")
+asynSetOption("serial1",0,"clocal","N")
+asynSetOption("serial1",0,"crtscts","N")
+#asynSetTraceIOMask("serial1",0, ESCAPE|HEX)
+#asynSetTraceMask("serial1", 0, ERROR|DRIVER|FLOW)
+
+# Load asyn records on all serial ports
+dbLoadTemplate("asynRecord.template")
+dbLoadRecords("$(IP)/db/VCMD1.db","P=$(PREFIX),PORT=serial1")
---
+++ st.cmd
@@ -0,0 +1,35 @@
+errlogInit(5000)
+< envPaths
+# Tell EPICS all about the record types, device-support modules, drivers,
+# etc. in this build from CARS
+dbLoadDatabase("../../dbd/CARSWin32.dbd")
+CARSWin32_registerRecordDeviceDriver(pdbbase)
+
+epicsEnvSet ("STREAM_PROTOCOL_PATH", "$(IP)/db")
+epicsEnvSet("PREFIX", "SLAC:Vcmd1:")
+epicsEnvSet("PORT", "serial1")
+
+
+################################################################################
+
+
+### Scan-support software
+# crate-resident scan.  This executes 1D, 2D, 3D, and 4D scans, and caches
+# 1D data, but it doesn't store anything to disk.  (You need the data catcher
+# or the equivalent for that.)  This database is configured to use the
+# "alldone" database (above) to figure out when motors have stopped moving
+# and it's time to trigger detectors.
+
+< serial.cmd
+#asynSetTraceIOMask("serial1",0,2)
+#asynSetTraceMask("serial1",0,9)
+
+
+dbLoadRecords("$(AUTOSAVE)/db/save_restoreStatus.db", "P=SLAC:Vcmd1:")
+dbLoadRecords("$(ASYN)/db/asynRecord.db", "P=$(PREFIX),R=asyn1,PORT=serial1,ADDR=0,IMAX=80,OMAX=80")
+
+
+iocInit
+
+# save other things every thirty seconds
+create_monitor_set("auto_settings.req", 30, "P=$(PREFIX)")
